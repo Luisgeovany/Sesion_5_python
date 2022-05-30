6419a84602a927930b30a966d7b8d3c16bc225f9
PERSONAL <glopezvan@gmail.com>
2022-05-30 17:50:07 -0500

    Se realiza el ejercicio 2 => numero primo

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,4 +1,18 @@
-#Escribe una función que pueda decirte si un número (número entero) es primo o no.
+# Escribe una función que pueda decirte si un número (número entero) es primo o no.
 
 def numeroPrimo():
-    pass
+    numero = int(input('Ingrese un numero: '))
+
+    if numero > 1:
+        for i in range(2, int(numero)):
+            if (int(numero) % i) == 0:
+                print(f'El numero {numero} NO ES PRIMO')
+                break
+            else:
+                print(f'El numero {numero} ES PRIMO')
+
+    else:
+        print(f'Ingrese un numero mayor 1 ')
+
+
+print(numeroPrimo())
